5668b7641f60ce2ae4c6e6e3627d41be52e2e2e5
conorbot <cwoodhayes@gmail.com>
2025-10-17 00:12:59 -0500

    out of curiosity, did a wrench transform into body frame

--- coursera_ch5.py
+++ coursera_ch5.py
@@ -1,4 +1,4 @@
-from modern_robotics import JacobianSpace, JacobianBody
+from modern_robotics import JacobianSpace, JacobianBody, Adjoint
 import numpy as np
 
 from lib.printers import print_readable_and_answer, print_readable
@@ -6,7 +6,7 @@ from lib.printers import print_readable_and_answer, print_readable
 
 def q1():
     print("################## Q1")
-    r = np.array([1 + np.cos(np.pi / 4), np.sin(np.pi / 4), 0])
+    r = np.array([0, 0, 0])
     f = np.array([2, 0, 0])
     p = np.cross(r, f)
 
@@ -28,6 +28,23 @@ def q1():
     tau = J_s.T @ F_s_col
 
     print_readable_and_answer(tau.flatten(), "tau")
+
+    return
+
+    # for my own curiosity, i want to figure out what this wrench is when expressed in the body frame
+    # like should it go up and to the right, so that the line of force goes thru the origin? so no moment,
+    # but an angled force?
+    Tsb = np.array(
+        [
+            [1, 0, 0, 2 + np.cos(np.pi / 4)],
+            [0, 1, 0, np.sin(np.pi / 4)],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+    )
+    Ad_Tbs = Adjoint(np.linalg.inv(Tsb))
+    F_b = Ad_Tbs.T @ F_s
+    print_readable_and_answer(F_b, "F_b (body wrench)")
 
 
 def q2():
@@ -93,8 +110,31 @@ def q4():
     print_readable_and_answer(J, "Jacobian")
 
 
+def q5q6():
+    print("################## Q5")
+
+    J_bv = np.array(
+        [
+            [-0.104, 0, 0.006, -0.045, 0, 0.006, 0],
+            [-0.889, 0.006, 0, -0.844, 0.006, 0, 0],
+            [0, -0.105, 0.889, 0, 0, 0, 0],
+        ]
+    )
+    A = J_bv @ J_bv.T
+    eigs = np.linalg.eig(A)
+
+    print(eigs.eigenvalues)
+    print(eigs.eigenvectors)
+
+    # eigenvalue idx 1 is the largest (by inspection of the above)
+    print_readable_and_answer(eigs.eigenvectors[1], "principal axis unit vec")
+
+    print_readable_and_answer(np.sqrt(eigs.eigenvalues[1]), "principal axis length")
+
+
 if __name__ == "__main__":
-    # q1()
+    q1()
     # q2()
     # q3()
-    q4()
+    # q4()
+    # q5q6()
