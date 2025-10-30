561636b48d719f4113aad103c6b5f7d4bf902928
conorbot <cwoodhayes@gmail.com>
2025-10-30 17:41:25 -0500

    added plots

--- assignment_3.py
+++ assignment_3.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 from pathlib import Path
+import signal
 
 from modern_robotics import se3ToVec, MatrixLog6, TransInv, FKinBody, JacobianBody
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 
 from lib.printers import print_readable_and_answer, print_readable
 
@@ -31,7 +34,9 @@ def display_iter(iter: IKIterT) -> None:
     print()
 
 
-def IKinBodyIterates(Blist, M, T, thetalist0, eomg, ev, maxiterations: int = 20):
+def IKinBodyIterates(
+    Blist, M, T, thetalist0, eomg, ev, maxiterations: int = 20
+) -> tuple[np.ndarray, bool, np.ndarray, np.ndarray, np.ndarray]:
     """Computes inverse kinematics in the body frame for an open chain robot
 
     Based on modern_robotics.IKinBody()
@@ -82,28 +87,69 @@ def IKinBodyIterates(Blist, M, T, thetalist0, eomg, ev, maxiterations: int = 20)
     i = 0
     T_i = TransInv(FKinBody(M, Blist, thetalist))
     Vb = se3ToVec(MatrixLog6(np.dot(T_i, T)))
-    err = (
-        np.linalg.norm([Vb[0], Vb[1], Vb[2]]) > eomg
-        or np.linalg.norm([Vb[3], Vb[4], Vb[5]]) > ev
-    )
+    err_wb = np.linalg.norm([Vb[0], Vb[1], Vb[2]])
+    err_vb = np.linalg.norm([Vb[3], Vb[4], Vb[5]])
+    err = (err_wb > eomg, err_vb > ev)
+
+    all_thetas = [thetalist]
+    all_err_wb = [err_wb]
+    all_err_vb = [err_vb]
+
     while err and i < maxiterations:
         thetalist = thetalist + np.dot(
             np.linalg.pinv(JacobianBody(Blist, thetalist)), Vb
         )
         # keep thetas in (-pi, pi]
         thetalist = np.atan2(np.sin(thetalist), np.cos(thetalist))
+        all_thetas.append(thetalist)
 
         T_i = TransInv(FKinBody(M, Blist, thetalist))
         Vb = se3ToVec(MatrixLog6(np.dot(T_i, T)))
         err_wb = np.linalg.norm([Vb[0], Vb[1], Vb[2]])
         err_vb = np.linalg.norm([Vb[3], Vb[4], Vb[5]])
         err = err_wb > eomg or err_vb > ev
+        all_err_vb.append(err_vb)
+        all_err_wb.append(err_wb)
 
         iter = IKIterT(i, thetalist, T_i, Vb, float(err_wb), float(err_vb))
         display_iter(iter)
 
         i = i + 1
-    return (thetalist, not err)
+    return (
+        thetalist,
+        not err,
+        np.array(all_thetas),
+        np.array(all_err_wb),
+        np.array(all_err_vb),
+    )
+
+
+def plot_3d_traj(
+    thetas: np.ndarray,
+    final_position: np.ndarray,
+    initial_guess: np.ndarray,
+    ax: Axes,
+    label: str,
+) -> None:
+    ax.plot(thetas[:, 0], thetas[:, 1], zs=thetas[:, 2], zdir="z", label=label)
+    ax.scatter(
+        [initial_guess[0]],
+        [initial_guess[1]],
+        [initial_guess[2]],
+        c="orange",
+        label="initial guess",
+    )
+    ax.scatter(
+        [final_position[0]],
+        [final_position[1]],
+        [final_position[2]],
+        c="black",
+        marker="x",
+        label="final position",
+    )
+    ax.set_xlabel("x (m)")
+    ax.set_ylabel("y (m)")
+    ax.set_zlabel("z (m)")  # type: ignore
 
 
 def main():
@@ -146,8 +192,8 @@ def main():
 
     ## Actual runs
 
-    theta_short_iterates = np.array([0, 0, 0, 0, 0, 0])
-    thetalist, success = IKinBodyIterates(
+    theta_short_iterates = np.array([1.554, -0.497, 1.244, 0.870, 0.000, 3.171])
+    thetalist, success, all_thetas, all_errw, all_errv = IKinBodyIterates(
         Blist, M, T_sd, theta_short_iterates, e_w, e_v
     )
     print_readable(
@@ -155,6 +201,22 @@ def main():
         f"theta_d short ({'CONVERGED' if success else 'NO CONVERGENCE'})",
         ndigits=4,
     )
+    fig = plt.figure("3d_traj_plot")
+    ax = fig.add_subplot(projection="3d")
+    plot_3d_traj(all_thetas, thetalist, theta_short_iterates, ax, "long")
+    ax.legend()
+
+    # plot the others:
+    fig2 = plt.figure("errs")
+    axw, axv = fig2.subplots(2, 1)
+    axw.plot(range(len(all_errw)), all_errw)
+    axw.set_xlabel("iteration")
+    axw.set_ylabel("Angular error magnitude (rads)")
+    axv.plot(range(len(all_errv)), all_errv)
+    axv.set_xlabel("iteration")
+    axv.set_ylabel("Linear error magnitude (m)")
+    plt.show()
+    return
 
     theta_long_iterates = np.array([0, 0, 0, 0, 0, 0])
     thetalist, success = IKinBodyIterates(Blist, M, T_sd, theta_long_iterates, e_w, e_v)
@@ -166,4 +228,6 @@ def main():
 
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
     main()
