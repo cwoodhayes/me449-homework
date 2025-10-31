ae4bda86cd4b2ea35acc2a2a57c9cb408f16138f
conorbot <cwoodhayes@gmail.com>
2025-10-31 10:51:01 -0500

    adding reqs.txt

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# {Repo Name}
+# Code for ME449 Assignment 3
+
+## run instructions:
+```
+
 
 Author: Conor Hayes
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from lynch-python-work!")
-
-
-if __name__ == "__main__":
-    main()
